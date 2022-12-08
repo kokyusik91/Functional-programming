749c8c5b0f2ae9a9d27ea4f11dac95b1cf6f3e3a
kks91 <kokyusik91@naver.com>
2022-12-08 13:56:08 +0900

    🎨 기존 장바구니 배열에서 객체로 변경

--- callBack.ts
+++ callBack.ts
@@ -19,6 +19,8 @@ const cart = [
   { name: 'shoes', price: 10 },
 ]
 
+const customSet = new Set(cart)
+
 // 개별 하나의 아이템
 const item = { name: 'ps5', price: 20 }
 
@@ -29,26 +31,31 @@ const cart2 = {
 }
 
 function objectSet<T>(object: any, key: string, value: T) {
-  const copy = Object.assign({}, object)
+  // const copy = Object.assign({}, object)
+  const copy = object.entries()
+  console.log('copy', copy)
   copy[key] = value
   return copy
 }
 
-const hello = objectSet(cart2, 'ps5', item)
-console.log('hello', hello)
-const hello2 = objectSet(cart2, 't-shirt', { name: 't-shirt', price: 99 })
-console.log('hello2', hello2)
+const copy = objectSet(customSet, 'socks', 22)
+console.log(copy)
 
-function setPriceByName(cart: any, name: any, field: any, value: any) {
-  const item = cart[name]
-  // 새로운 아이템을 만들고
-  const newItem = objectSet(item, field, value)
-  // 기존 카트 객체에 추가한다.
-  const newCart = objectSet(cart, name, newItem)
-  return newCart
-}
+// const hello = objectSet(cart2, 'ps5', item)
+// console.log('hello', hello)
+// const hello2 = objectSet(cart2, 't-shirt', { name: 't-shirt', price: 99 })
+// console.log('hello2', hello2)
 
-let updateCart = setPriceByName(cart2, 'socks', 'quantity', 35)
-console.log(updateCart)
-updateCart = setPriceByName(cart2, 'socks', 'tax', 2.34)
-console.log(updateCart)
+// function setPriceByName(cart: any, name: any, field: any, value: any) {
+//   const item = cart[name]
+//   // 새로운 아이템을 만들고
+//   const newItem = objectSet(item, field, value)
+//   // 기존 카트 객체에 추가한다.
+//   const newCart = objectSet(cart, name, newItem)
+//   return newCart
+// }
+
+// let updateCart = setPriceByName(cart2, 'socks', 'quantity', 35)
+// console.log(updateCart)
+// updateCart = setPriceByName(cart2, 'socks', 'tax', 2.34)
+// console.log(updateCart)
